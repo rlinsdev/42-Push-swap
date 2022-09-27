d0709ebba39154ae4f611ee33cd0a44df3e3b68f
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-27 08:14:56 -0300

    Radix Sample 1

--- src/samp_Radix.c
+++ src/samp_Radix.c
@@ -6,38 +6,79 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:13:45 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/26 14:14:48 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/27 08:14:02 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//https://www.freecodecamp.org/portuguese/news/algoritmos-de-ordenacao-explicados-com-exemplos-em-python-java-e-c/
-void countsort(int arr[],int n,int place){
+#include <push_swap.h>
 
-		//o intervalo para números inteiros é 10, pois os dígitos variam de 0 a 9
-        int i,freq[range]={0};
-        int output[n];
-
-        for(i=0;i<n;i++)
-                freq[(arr[i]/place)%range]++;
-
-        for(i=1;i<range;i++)
-                freq[i]+=freq[i-1];
-
-        for(i=n-1;i>=0;i--){
-                output[freq[(arr[i]/place)%range]-1]=arr[i];
-                freq[(arr[i]/place)%range]--;
-        }
-
-        for(i=0;i<n;i++)
-                arr[i]=output[i];
+static int	get_max (int a[], int n)
+{
+	int max = a[0];
+	for (int i = 1; i < n; i++)
+		if (a[i] > max)
+			max = a[i];
+	return (max);
 }
 
-void radixsort(ll arr[],int n,int maxx){            //maxx é o elemento máximo do array
+static void	radix_sort (int a[], int n)
+{
+	int bucket[10][10], bucket_cnt[10];
+	int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+	lar = get_max (a, n);
+	while (lar > 0)
+	{
+		NOP++;
+		lar /= 10;
+	}
+	for (pass = 0; pass < NOP; pass++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			bucket_cnt[i] = 0;
+		}
+		for (i = 0; i < n; i++)
+		{
+			r = (a[i] / divisor) % 10;
+			bucket[r][bucket_cnt[r]] = a[i];
+			bucket_cnt[r] += 1;
+		}
+		i = 0;
+		for (k = 0; k < 10; k++)
+		{
+			for (j = 0; j < bucket_cnt[k]; j++)
+			{
+				a[i] = bucket[k][j];
+				i++;
+			}
+		}
+		divisor *= 10;
+		printf ("After pass %d : ", pass + 1);
+		for (i = 0; i < n; i++)
+			printf ("%d ", a[i]);
+		printf ("\n");
+	}
+}
 
-        int mul=1;
-        while(maxx){
-                countsort(arr,n,mul);
-                mul*=10;
-                maxx/=10;
-        }
+void radixsort()
+{
+	int i = 0;
+	// int count = 9;
+	// int arrNumber[] = {3, 2, 1, 6, 5, 4, 9, 8, 7};
+	int count = 3;
+	int arrNumber[] = {35, 22, 109};
+
+	// int i, n, a[10];
+	// scanf ("%d", &n);
+	// printf ("Enter items: ");
+	// for (i = 0; i < n; i++)
+	// {
+	// 	scanf ("%d", &a[i]);
+	// }
+	radix_sort (arrNumber, count);
+	printf("Sorted items : ");
+	for (i = 0; i < count; i++)
+		printf ("%d ", arrNumber[i]);
+	printf ("\n");
+	// return (0);
 }
