32dbc64969d28efa7be202f0a630d80c4848ccbc
Rodrigo Lins <rlinsdev@gmail.com>
2022-09-27 07:28:03 -0300

    dinamically insert item

--- src/samp_LinkedList.c
+++ src/samp_LinkedList.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 21:16:55 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/27 07:09:18 by rlins            ###   ########.fr       */
+/*   Updated: 2022/09/27 07:27:13 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,24 @@ node_t *create_new_node(int value)
 	return result;
 }
 
+node_t *insert_at_head(node_t *head, node_t *node_to_insert)
+{
+	node_to_insert->next = head;
+	return (node_to_insert);
+}
+
 void linked_list()
 {
-	node_t *head;
+	node_t *head = NULL;
 	node_t *tmp;
 
-	tmp = create_new_node(32);
-	head = tmp;
-	// free(tmp);
-	tmp = create_new_node(8);
-	head = tmp;
-	// free(tmp);
-	tmp = create_new_node(34);
-	head = tmp;
+	for (int i = 0; i < 5; i++)
+	{
+		tmp = create_new_node(i);
+		head = insert_at_head(head, tmp);
+		// tmp->next = head;
+		// head = tmp;
+	}
 
 	print_list(head);
 	free(tmp);
