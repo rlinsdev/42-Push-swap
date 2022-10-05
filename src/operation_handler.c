1231d0af228a2088f624b389e13286f23c1b46a9
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-05 07:51:33 -0300

    spliting handler operation in files

--- src/operation_handler.c
+++ src/operation_handler.c
@@ -6,39 +6,11 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 07:16:28 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/04 19:40:13 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/05 07:05:37 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
-
-// // TODO
-// void	swap_a (t_node **node_a, t_node **node_b)
-// { }
-// void	swap_b (t_node **node_a, t_node **node_b)
-// { }
-// void	swap_swap (t_node **node_a, t_node **node_b)
-// {
-// 	swap_a(*node_a, *node_b);
-// 	swap_b(*node_a, *node_b);
-// }
-
-// TODO
-void	push_a (t_node **node_a, t_node **node_b)
-{ }
-
-// TODO
-void	push_b (t_node **node_a, t_node **node_b)
-{
-	// t_node *aux;
-
-	// aux = *node_a;
-	// node_a = node_b;
-
-	// node_b = aux;
-}
-
-
 
 // TODO
 void	reverse_rotate_a (t_node **node_a, t_node **node_b)
