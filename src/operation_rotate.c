1a7008eed40e1b3fa7509b6484abd6c64072d8fa
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-13 11:01:50 -0300

    Pointer to pointer in rotate

--- src/operation_rotate.c
+++ src/operation_rotate.c
@@ -6,41 +6,48 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 19:33:52 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/05 23:09:55 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/13 10:56:18 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-static void	rotate_stack(t_stack *stack);
+static void	rotate_stack(t_stack **stack);
 
 // TODO
-void	rotate_a(t_stack *stack_a)
+void	rotate_a(t_stack **stack_a)
 {
 	rotate_stack(stack_a);
 	write(1, "ra\n", 3);
+	test_print_list(*stack_a);
 }
 
-void	rotate_b(t_stack *stack_b)
+void	rotate_b(t_stack **stack_b)
 {
-	rotate_stack(stack_b);
+	// rotate_stack(stack_b);
 	write(1, "rb\n", 3);
 }
 
-void	rotate_rotate(t_stack *stack_a, t_stack *stack_b)
+void	rotate_rotate(t_stack **stack_a, t_stack **stack_b)
 {
-	rotate_stack(stack_a);
-	rotate_stack(stack_b);
+	// rotate_stack(stack_a);
+	// rotate_stack(stack_b);
 	write(1, "rr\n", 3);
 }
 
-static void	rotate_stack(t_stack *stack)
+/**
+ * @brief Stack will receive the pointer to the next. The last one will receive
+ * the temp variable
+ * @param stack
+ */
+static void	rotate_stack(t_stack **stack)
 {
-	t_stack	*aux;
+	t_stack	*tmp;
+	t_stack	*tail;
 
-	aux = stack;
-	if (aux == NULL)
-		return ;
-	aux = aux->next;
-	stack = aux;
+	tmp = *stack;
+	*stack = (*stack)->next;
+	tail = get_last_stack(*stack);
+	tmp->next = NULL;
+	tail->next = tmp;
 }
