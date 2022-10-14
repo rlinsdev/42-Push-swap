adf9b3be2f644e0809a7f88bccedee25da812356
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-14 11:52:43 -0300

    removing comments

--- apps/app.c
+++ apps/app.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/09/28 07:08:54 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/14 07:21:36 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,5 @@
  */
 int	main(int argc, char *argv[])
 {
-	// return(start(argc, argv));
-	// quick();
-	// merge_main();
-	// linked_list();
-	// radixsort();
-	// radixsort2();
-	// radixsort3();
-
 	return (start(argc, argv));
 }
