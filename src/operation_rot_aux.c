5ef17fc1971514f12579da375a1e1a988b550477
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-18 07:15:46 -0300

    Commenting - Aux Rotation

--- src/operation_rot_aux.c
+++ src/operation_rot_aux.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 15:11:46 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/16 09:00:41 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/18 07:15:23 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	aux_rev_rotate_both(t_stack **stack_a, t_stack **stack_b, int *cost_a,
 						int *cost_b)
 {
+	// Just when both have negative cost, then we will reverse rotate both
 	while (*cost_a < 0 && *cost_b < 0)
 	{
 		(*cost_a)++;
@@ -36,6 +37,7 @@ void	aux_rotate_both(t_stack **stack_a, t_stack **stack_b, int *cost_a,
 
 void	aux_rotate_a(t_stack **stack_a, int *cost)
 {
+	// If exist any cost, rotate A until let in head the correct value
 	while (*cost)
 	{
 		if (*cost > 0)
@@ -43,6 +45,7 @@ void	aux_rotate_a(t_stack **stack_a, int *cost)
 			rotate_a(stack_a);
 			(*cost)--;
 		}
+		// If cost is negative, Reverse rotate
 		else if (*cost < 0)
 		{
 			r_rotate_a(stack_a);
@@ -53,6 +56,7 @@ void	aux_rotate_a(t_stack **stack_a, int *cost)
 
 void	aux_rotate_b(t_stack **stack_b, int *cost)
 {
+	// If exist any cost, rotate B until let in head the correct value
 	while (*cost)
 	{
 		if (*cost > 0)
@@ -60,6 +64,7 @@ void	aux_rotate_b(t_stack **stack_b, int *cost)
 			rotate_b(stack_b);
 			(*cost)--;
 		}
+		// If cost is negative, Reverse rotate
 		else if (*cost < 0)
 		{
 			r_rotate_b(stack_b);
