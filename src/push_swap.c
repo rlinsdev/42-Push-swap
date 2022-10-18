35fc6c49c783dc80bf6e7c7ed7cae78a8f14b796
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-18 12:08:18 -0300

    Put correct error message

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 07:17:21 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/18 11:38:56 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/18 12:07:30 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	start(int argc, char **argv)
 	{
 		if (invalid_input(argv, argc))
 		{
-			ft_printf("Invalid arguments\n");
+			ft_printf("Error\n");
 			exit (1);
 		}
 		// Initialize Stack_b
